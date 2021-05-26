d11b024ec1f0f05081b10b9f1a01f80c7c0f8e68
Andrii-Batoh <batoh.dev@gmail.com>
2021-05-26 17:52:59 +0300

    Fix next when finish

--- src/components/store/index.js
+++ src/components/store/index.js
@@ -1,13 +1,22 @@
 import {combineReducers, createStore, applyMiddleware} from "redux";
 import {initData} from "../initialData/init";
 import {selectedSection, signs, i18n, question} from "../reducers/reducers";
+import {NEXT_QUESTION} from "../../constants";
 const stateData = {...initData};
 const logger = store => next => action => {
     let result;
+    const prev = store.getState();
     console.groupCollapsed('dispatching', action.type);
     console.log('prev state', store.getState());
     console.log('action', action);
-    result = next(action);
+    switch (action.type) {
+        case NEXT_QUESTION:
+            if(prev.signs[action.section].length === 1) {
+                return
+            }
+        default:
+            result = next(action);
+    }
     console.log('next state', store.getState());
     console.groupEnd();
 };
